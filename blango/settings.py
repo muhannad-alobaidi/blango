6f535aa256c8d04adb8fc4948b7b02c74f9d0a34
Muhannad Alobaidi <muhannad.alobaidi@yahoo.com>
2025-02-09 07:54:16 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -56,12 +56,17 @@ class Dev(Configuration):
         'django.contrib.contenttypes',
         'django.contrib.sessions',
         'django.contrib.messages',
+        'django.contrib.sites',
         'django.contrib.staticfiles',
         'crispy_forms',
         'crispy_bootstrap5',
         'blog',
         'debug_toolbar',
         'blango_auth',
+        "allauth", 
+        "allauth.account", 
+        "allauth.socialaccount", 
+        "allauth.socialaccount.providers.google",
     ]
 
     MIDDLEWARE = [
@@ -77,6 +82,11 @@ class Dev(Configuration):
 
     ROOT_URLCONF = 'blango.urls'
     INTERNAL_IPS = ["192.168.11.51"]
+    SITE_ID = 1
+    ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_USERNAME_REQUIRED = False
+    ACCOUNT_AUTHENTICATION_METHOD = "email"
 
     TEMPLATES = [
         {
