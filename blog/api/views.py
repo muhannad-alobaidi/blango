a12fe368e06a964ffd8bdf746b8c513597c09e0d
Muhannad Alobaidi <muhannad.alobaidi@yahoo.com>
2025-03-01 12:59:43 +0000

    Finish filtering

--- blog/api/views.py
+++ blog/api/views.py
@@ -13,10 +13,52 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.exceptions import PermissionDenied 
 
+from django.db.models import Q
+from django.utils import timezone
+from datetime import timedelta
+from django.http import Http404
+
+
+
 
 class PostViewSet(viewsets.ModelViewSet):
   permissions = [AuthorModifyOrReadOnly, IsAdminUserForObject]
   queryset = Post.objects.all()
+
+  def get_queryset(self):
+      if self.request.user.is_anonymous:
+          # published only
+          queryset = self.queryset.filter(published_at__lte=timezone.now())
+
+      elif not self.request.user.is_staff:
+          # allow all
+          queryset = self.queryset
+      else:
+          queryset = self.queryset.filter(
+              Q(published_at__lte=timezone.now()) | Q(author=self.request.user)
+          )
+
+      time_period_name = self.kwargs.get("period_name")
+
+      if not time_period_name:
+          # no further filtering required
+          return queryset
+
+      if time_period_name == "new":
+          return queryset.filter(
+              published_at__gte=timezone.now() - timedelta(hours=1)
+          )
+      elif time_period_name == "today":
+          return queryset.filter(
+              published_at__date=timezone.now().date(),
+          )
+      elif time_period_name == "week":
+          return queryset.filter(published_at__gte=timezone.now() - timedelta(days=7))
+      else:
+          raise Http404(
+              f"Time period {time_period_name} is not valid, should be "
+              f"'new', 'today' or 'week'"
+          )
 
   def get_serializer_class(self):
       if self.action in ("list", "create"):
@@ -24,7 +66,7 @@ class PostViewSet(viewsets.ModelViewSet):
       return PostDetailSerializer
 
   @method_decorator(cache_page(300))
-  @method_decorator(vary_on_headers("Authorization"))
+  @method_decorator(vary_on_headers("Authorization", "Cookie"))
   @method_decorator(vary_on_cookie)
   @action(methods=["get"], detail=False, name="Post by the logged in user")
   def mine(self, request):
